474fe90ce69a15ec8f7573c531540ce283fc470b
vidurasilva <kvidurachathuranga@gmail.com>
2020-02-17 22:55:29 +0530

    Reed Text file

---
+++ Go_Project.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	/*Reade two file*/
+	data, err := ioutil.ReadFile("D:/Project/GO/Go_app/Data_File.txt")
+	/*check errors*/
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	fmt.Println("Contents of file:", string(data))
+}
